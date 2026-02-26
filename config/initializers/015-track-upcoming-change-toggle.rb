5a5bb595b2d43a4d37fb2121c7f1474c0a43e2f3
Martin Brennan <martin@discourse.org>
2026-02-26 17:17:54 +1000

    FEATURE: Change TL3 requirement caps
    WIP

--- config/initializers/015-track-upcoming-change-toggle.rb
+++ config/initializers/015-track-upcoming-change-toggle.rb
@@ -18,8 +18,14 @@ Rails.application.config.after_initialize { UpcomingChanges.clear_caches! }
 
 DiscourseEvent.on(:upcoming_change_enabled) do |setting_name|
   # Respond to event here, e.g. if setting_name == :enable_form_templates do X.
+  if setting_name == :change_trust_level_3_cap_requirements
+    SiteSetting::Action::ChangeTrustLevel3CapRequirementsToggled.call(enabled: true)
+  end
 end
 
 DiscourseEvent.on(:upcoming_change_disabled) do |setting_name|
   # Respond to event here, e.g. if setting_name == :enable_form_templates do X.
+  if setting_name == :change_trust_level_3_cap_requirements
+    SiteSetting::Action::ChangeTrustLevel3CapRequirementsToggled.call(enabled: false)
+  end
 end
